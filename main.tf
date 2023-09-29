954ea8a9fe4432ebe00964753315eb6854e9afc2
lalita2407 <lalitalallu02@gmail.com>
2023-09-29 14:23:39 +0530

    small fix

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "azurerm_lb_rule" "lb_rule" {
   protocol                       = "Tcp"
   frontend_port                  = 443
   backend_port                   = 443
-  frontend_ip_configuration_name = "${var.name}-pubIP"
+  frontend_ip_configuration_name = "${var.name}-lb_front"
   probe_id                       = azurerm_lb_probe.lb_probe.id
   backend_address_pool_ids       = [azurerm_lb_backend_address_pool.backend_pool.id]
 }
